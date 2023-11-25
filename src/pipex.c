22aa049ecbf9f9d75809557dc10aa3bc0c36f1d1
SrRecursive <rojohnibana@gmail.com>
2023-11-25 16:30:48 +0100

    Removed unnecesary parser

--- src/pipex.c
+++ src/pipex.c
@@ -6,25 +6,11 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 14:49:56 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/11/25 00:56:26 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/11/25 16:19:18 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
-
-static int	ft_argv_parser(int argc, char **argv)
-{
-	int	counter;
-
-	counter = 2;
-	while (argv[argc - counter] && (argc - counter) > 1)
-	{
-		if (!argv[argc - counter][0])
-			return (1);
-		counter++;
-	}
-	return (0);
-}
 
 int	pipex(int argc, char **argv, char **envp)
 {
@@ -33,8 +19,6 @@ int	pipex(int argc, char **argv, char **envp)
 	pid_t	pid;
 
 	status = 0;
-	if (ft_argv_parser(argc, argv) != 0)
-		ft_error_message("Empty command");
 	if (pipe(pipefd) < 0)
 		ft_error_message("pipe");
 	pid = fork();
@@ -56,6 +40,8 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc != 5)
 		return (1);
+	for (int i = 0; argv[i]; i++)
+		printf("%s\n", argv[i]);
 	status = pipex(argc, argv, envp);
 	return (status);
 }
